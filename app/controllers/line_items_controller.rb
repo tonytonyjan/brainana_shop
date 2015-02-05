8dfa56a5f50a8d7851200970df8f772ee587f265
Jian Weihang <tonytonyjan@gmail.com>
2015-02-05 16:42:56 +0800

    use case: remove from cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -6,4 +6,11 @@ class LineItemsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     redirect_to request.referer, alert: '找不到該產品'
   end
+
+  def destroy
+    current_cart.line_items.find(params[:id]).destroy
+    redirect_to cart_path
+  rescue ActiveRecord::RecordNotFound
+    redirect_to request.referer, alert: '購物車內找不到該產品'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   resources :products, :orders, only: %i[show index]
   resource :cart, only: %i[show]
-  resources :line_items, only: %i[create] # add to cart
+  resources :line_items, only: %i[create destroy] # add to cart
   namespace :admin do
     resources :products, :carts
     resources :orders, :transactions, only: %i[show index]
