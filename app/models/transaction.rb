2854911ebc536001a1d0c1203ac25514ce8d7f6c
Jian Weihang <tonytonyjan@gmail.com>
2015-02-07 10:45:55 +0800

    generate trade_number if it does not exist

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,10 +2,14 @@ class Transaction < ActiveRecord::Base
   belongs_to :order
   after_initialize :generate_trade_number
 
+  def to_param
+    trade_number
+  end
+
 private
 
   def generate_trade_number
-    self.trade_number = SecureRandom.hex(3)
+    self.trade_number = SecureRandom.hex(3) if trade_number.nil?
   end
 end
 # PaymentDate: 2015/02/06 07:40:09
