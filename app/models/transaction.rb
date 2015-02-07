a7957903217396f1e3bd6426aa68c61d5684a051
Jian Weihang <tonytonyjan@gmail.com>
2015-02-07 23:27:27 +0800

    finish trasaction test

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,7 @@
 class Transaction < ActiveRecord::Base
   belongs_to :order
   after_initialize :generate_trade_number
+  validate :check_trade_number, :check_mac_value, on: :update
 
   def to_param
     trade_number
@@ -10,5 +11,13 @@ private
 
   def generate_trade_number
     self.trade_number = SecureRandom.hex(3) if trade_number.nil?
+  end
+
+  def check_trade_number
+    errors.add(:params, 'wrong trade number') unless params['MerchantTradeNo'] == trade_number
+  end
+
+  def check_mac_value
+    errors.add(:params, 'wrong mac value') unless Allpay.client.verify_mac(params)
   end
 end
--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -5,4 +5,24 @@ class TransactionTest < ActiveSupport::TestCase
     t = Transaction.new
     assert_match(/\w{6}/, t.trade_number)
   end
+
+  test '避免存入錯誤的交易資訊' do
+    t = transactions(:one)
+    assert_raise(ActiveRecord::RecordInvalid) do
+      t.update! params: {
+        RtnCode: '1',
+        PaymentType: 'Credit_CreditCard',
+        TradeAmt: '700',
+        PaymentTypeChargeFee: '14',
+        PaymentDate: '2015/02/07 14:21:00',
+        SimulatePaid: '0',
+        CheckMacValue: '3AF270CCCFA58CA0349F4FD462E21643',
+        TradeDate: '2015/02/07 14:20:47',
+        MerchantID: '2000132',
+        TradeNo: '1502071420478656',
+        RtnMsg: '交易成功',
+        MerchantTradeNo: '355313xx'
+      }
+    end
+  end
 end
