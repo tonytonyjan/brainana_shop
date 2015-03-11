a7f0d54fc0881509d402c1a3f7088d7bf435b6ee
Jian Weihang <tonytonyjan@gmail.com>
2015-03-11 17:45:38 +0800

    use allpay_client 2.0 feature

--- app/models/allpay/form.rb
+++ app/models/allpay/form.rb
@@ -1,23 +1,18 @@
 module Allpay
   class Form
-    def initialize transaction, **override_params
-      @transaction, @override_params = transaction, override_params
+    def initialize transaction, **overwrite_params
+      @transaction, @overwrite_params = transaction, overwrite_params
     end
 
     def params
-      ret = {
-        MerchantID: Settings.allpay.merchant_id,
+      Allpay.client.generate_checkout_params({
         MerchantTradeNo: @transaction.trade_number,
-        MerchantTradeDate: Time.now.strftime('%Y/%m/%d %H:%M:%S'),
-        PaymentType: 'aio',
         TotalAmount: @transaction.order.price.to_i,
         TradeDesc: Settings.allpay.trade_desc,
         ItemName: item_name,
         ReturnURL: Settings.allpay.return_url,
         ChoosePayment: 'Credit'
-      }.merge!(@override_params)
-      ret[:CheckMacValue] = Allpay.client.make_mac(ret)
-      ret
+      }.merge!(@overwrite_params))
     end
 
     def item_name
